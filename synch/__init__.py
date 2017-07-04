aa671190f0fdf9e6b954d550bebb77d66726cae4
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-07-04 11:11:40 -0400

    fixes

--- synch/__init__.py
+++ synch/__init__.py
@@ -13,32 +13,36 @@ from __future__ import unicode_literals
 
 import hashlib
 
-from mo_dots import coalesce, listwrap
-from mo_files import File
-from mo_json import value2json
+from boto.s3 import connect_to_region
+from mo_dots import coalesce, listwrap, unwrap, Data
+from mo_files import File, join_path
+from mo_json import bytes2hex
 from mo_logs import startup, constants, Log, strings
+from mo_logs.strings import quote
 from mo_threads import Process
-from pyLibrary.aws import s3
-from pyLibrary.env import http
 from pyLibrary.queries import jx
 
+DEBUG = True
 CHUNK_SIZE = 8388608  # BE SURE THIS IS BOTO'S multipart_chunksize https://boto3.readthedocs.io/en/latest/reference/customizations/s3.html#boto3.s3.transfer.TransferConfig
 
 
 def md5(source, chunk_size=CHUNK_SIZE):
     md5s = []
-    for g, data in jx.groupby(source.read_bytes(), chunk_size):
+    for g, data in jx.groupby(source.read_bytes(), size=chunk_size):
         md5s.append(hashlib.md5(data).digest())
 
-    if len(md5s) > 1:
+    if len(md5s) == 1:
+        return quote(md5s[0].encode("hex"))
+    else:
         Log.warning("not known to work")
-
-    new_md5 = hashlib.md5(b"".join(md5s))
-    return unicode(new_md5.hexdigest()+b"-"+str(len(md5s)))
+        new_md5 = hashlib.md5(b"".join(md5s))
+        return unicode(new_md5.hexdigest()+b"-"+str(len(md5s)))
 
 
 def _synch(settings):
     cache = File(settings.local_cache)
+    if not cache.exists:
+        cache.create()
     settings.destination.directory = settings.destination.directory.trim("/")
 
     for repo in listwrap(coalesce(settings.repo, settings.repos)):
@@ -51,19 +55,33 @@ def _synch(settings):
         local_repo = File.new_instance(cache, name)
         local_dir = File.new_instance(local_repo, repo.source.directory)
         if not local_repo.exists:
-            Process("clone repo", ["git", "clone", repo.source.url], cwd=local_repo).service_stopped.wait()
+            Process("clone repo", ["git", "clone", repo.source.url, name], cwd=cache, shell=True, debug=DEBUG).join(raise_on_error=True)
         # SWITCH TO BRANCH
-        Process("checkout", ["git", "checkout", settings.branch], cwd=local_repo).service_stopped.wait()
+        Process("checkout", ["git", "checkout", repo.source.branch], cwd=local_repo, shell=True, debug=DEBUG).join(raise_on_error=True)
         # UPDATE THE LOCAL COPY
-        Process("update", ["git", "pull", "origin", settings.branch])
+        Process("update", ["git", "pull", "origin", repo.source.branch], cwd=local_repo, shell=True, debug=DEBUG).join(raise_on_error=True)
         # GET CURRENT LISTING OUT OF S3
-        bucket = s3.Bucket(kwargs=repo.destination)
-        remote_prefix = repo.directory.trim('/') + "/"
-        metas = {m.key[len[remote_prefix:]]: m for m in bucket.metas(prefix=remote_prefix)}
+
+        try:
+            connection = connect_to_region(
+                region_name=repo.destination.region,
+                calling_format="boto.s3.connection.OrdinaryCallingFormat",
+                aws_access_key_id=unwrap(repo.destination.aws_access_key_id),
+                aws_secret_access_key=unwrap(repo.destination.aws_secret_access_key)
+            )
+            bucket = connection.get_bucket(repo.destination.bucket)
+        except Exception as e:
+            Log.error("Problem connecting to {{bucket}}", bucket=repo.destination.bucket, cause=e)
+
+        remote_prefix = repo.destination.directory.strip('/') + "/"
+        listing = bucket.list(prefix=remote_prefix)
+        metas = {m.key[len(remote_prefix):]: Data(key=m.key, etag=m.etag) for m in listing}
         net_new = []
-        for local_file in File.new_instance(local_repo, settings.directory).children:
-            local_rel_file = local_file.abs_path[len(local_dir.abspath):]
-            remote_file = metas[local_rel_file]
+        for local_file in local_dir.leaves:
+            local_rel_file = local_file.abspath[len(local_dir.abspath):].lstrip(b'/')
+            if "/." in local_rel_file or local_rel_file.startswith("."):
+                continue
+            remote_file = metas.get(local_rel_file)
             if remote_file:
                 if remote_file.etag != md5(local_file):
                     net_new.append(local_file)
@@ -72,30 +90,23 @@ def _synch(settings):
 
         # SEND DIFFERENCES
         for n in net_new:
-            bucket_file = settings.destination.directory+"/"+n.abspath[len(local_dir.abspath):]
-            bucket.write(key=bucket_file, value=n.read_bytes(), disable_zip=True)
+            bucket_file = join_path(repo.destination.directory, n.abspath[len(local_dir.abspath):])
+            Log.note("upload {{file}}", file=bucket_file)
+            storage = bucket.new_key(bucket_file)
+            storage.set_contents_from_file(file(n.abspath))
+            storage.set_acl('public-read')
 
 
 def main():
     settings = startup.read_settings()
     Log.start(settings.debug)
-
     constants.set(settings.constants)
-    path = settings.elasticsearch.host + ":" + unicode(settings.elasticsearch.port)
 
     try:
         _synch(settings)
     except Exception as e:
-        Log.error("Problem with assign of shards", e)
+        Log.error("Problem with synch", e)
     finally:
-        for p, command in settings["finally"].items():
-            for c in listwrap(command):
-                response = http.put(
-                    path + p,
-                    data=value2json(c)
-                )
-                Log.note("Finally {{command}}\n{{result}}", command=c, result=response.all_content)
-
         Log.stop()
 
 
